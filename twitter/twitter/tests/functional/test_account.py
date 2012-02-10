290645c4ed45bbf0d25936e9079ee3157afc2b86
Chetan Sharma <chetan2309@gmail.com>
2012-02-10 12:05:17 +0530

    Revert "Comitting base code for Pylon twitter"
    This reverts commit 8d34b3453e5bb368b4341cde483fec3042b7e3f0.

--- twitter/twitter/tests/functional/test_account.py
+++
@@ -1,7 +0,0 @@
-from twitter.tests import *
-
-class TestAccountController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url_for(controller='account'))
-        # Test response...
