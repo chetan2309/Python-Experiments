bf7c33c076cf6c0ca9e8411ea09a2ad3a464c4b4
Chetan Sharma <chetan2309@gmail.com>
2012-04-18 12:22:54 +0530

    Deleting files

--- twitter/twitter/tests/functional/test_hello.py
+++
@@ -1,7 +0,0 @@
-from twitter.tests import *
-
-class TestHelloController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url_for(controller='hello'))
-        # Test response...
